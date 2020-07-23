6596bfe801c51286bbc0979242070c645e257328
kingpasu <68714638+kingpasu@users.noreply.github.com>
2020-07-23 18:17:58 -0400

    Add files via upload

---
+++ website.js
@@ -0,0 +1,29 @@
+function Memebutt() {
+var mbutt=document.getElementById("memebutt");
+mbutt.style.display = "none";
+document.getElementById("memegifs").style.display = "block";
+document.getElementById("bimg").style.backgroundImage ="url(vaporwave.gif)";
+document.getElementById("reset").style.display = "block";
+document.getElementById("mememusic").play();
+document.getElementById("nekodance").style.display="none";
+document.getElementById("pikachu").style.display="none";
+document.getElementById("gtr").style.display="none";
+}
+
+function hobbies(){
+	var hbutt=document.getElementById("hobbiesbutt");
+	hbutt.style.display ="none";
+	document.getElementById("tablebackground").style.display = "block";	
+}
+
+function interest(){
+	var ibutt=document.getElementById("interestbutt");
+	ibutt.style.display ="none";
+	document.getElementById("graphbackround").style.display = "block";	
+}
+
+function resume(){
+	var rbutt=document.getElementById("resumebutt");
+	rbutt.style.display ="none";
+	document.getElementById("resume").style.display = "block";	
+}
